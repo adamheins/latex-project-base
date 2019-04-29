52638c84e0759d6bbbdcaee6125eb3a36c243bd8
Adam Heins <mail@adamheins.com>
2019-04-29 15:05:19 -0400

    Start basic shell convenience functions.

---
+++ projres.sh
@@ -0,0 +1,13 @@
+proj_res_path=~/dev/proj/project-resources  # TODO use script path
+proj_res_tex_path=$proj_res_path/templates/tex
+
+texme() {
+  cp $proj_res_tex_path/bibliography.bib .
+  cp $proj_res_tex_path/commands.tex .
+  cp $proj_res_tex_path/content.tex .
+  cp $proj_res_tex_path/Makefile .
+}
+
+bibme() {
+  # TODO autocomplete bib entry types
+}
