0c6d5e2096dd579820ec698cf09af882e191c46c
Adam Heins <mail@adamheins.com>
2020-07-02 17:10:24 -0400

    use script path

--- latex-project-base.sh
+++ latex-project-base.sh
@@ -1,4 +1,4 @@
-proj_res_path=~/dev/proj/project-resources  # TODO use script path
+proj_res_path=~/sw/latex-project-base  # TODO use script path
 proj_res_tex_path=$proj_res_path/templates/tex/bare
 
 texme() {
